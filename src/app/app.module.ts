52be98d31e04968ff2693cadeac52430927f131e
Joao <j_donizetti@outlook.com>
2025-04-22 16:02:13 -0300

    chamando a pokeAPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 //components
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from '../../service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './poke-list.component.html',
   styleUrl: './poke-list.component.scss'
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
 
+  constructor(
+    private pokeApiService: PokeApiService
+  ){}
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => res
+    );
+  }
 }
