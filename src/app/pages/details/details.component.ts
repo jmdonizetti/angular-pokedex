ee7696bf7979bd23ba338f21de63a72a00196fe7
Joao <j_donizetti@outlook.com>
2025-04-25 18:04:08 -0300

    fazendo o ts do details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from '../../service/poke-api.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
 
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'
+
+
+  constructor(
+    private activetedRouter: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon(){
+    const id = this.activetedRouter.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
+  }
 }
