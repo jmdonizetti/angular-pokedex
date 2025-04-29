eadc6daab6e3f2dbaa2e8929f43df5ba04fec8bf
Joao <j_donizetti@outlook.com>
2025-04-29 11:09:10 -0300

    Criando Animações

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -10,7 +10,7 @@ import { map, tap } from 'rxjs/operators'
 })
 export class PokeApiService {
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
 
   constructor(
     private http: HttpClient
